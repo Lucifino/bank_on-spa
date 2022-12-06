e0c9be9a1d9b956588ea956ac1d55fe04f9de776
Guiliano Bastien <gpconcepcion1@up.edu.ph>
2022-12-06 15:40:04 +0800

    revert angular version

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -1,56 +1,25 @@
 import {NgModule} from '@angular/core';
-import { APOLLO_OPTIONS} from 'apollo-angular';
-import {ApolloClientOptions, ApolloLink, DefaultOptions, InMemoryCache} from '@apollo/client/core';
+import {ApolloModule, APOLLO_OPTIONS} from 'apollo-angular';
+import {ApolloClientOptions, InMemoryCache} from '@apollo/client/core';
 import {HttpLink} from 'apollo-angular/http';
 import {environment} from "../environments/environment";
-import {onError} from "@apollo/client/link/error";
 // // @ts-ignore
 // import extractFiles from 'extract-files/extractFiles.mjs';
 // // @ts-ignore
 // import isExtractableFile from 'extract-files/isExtractableFile.mjs';
+import {extractFiles, isExtractableFile} from 'extract-files';
 
 
 const uri = environment.graphql_url; // <-- add the URL of the GraphQL server here
-
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
-
-  const errorLink = onError(({ graphQLErrors, networkError }) => {
-    if (graphQLErrors) {
-
-    }
-    if (networkError) {
-    }
-  });
-  const link = ApolloLink.from([errorLink, httpLink.create({uri})]);
-
-  const defaultOptions: DefaultOptions = {
-    watchQuery: {
-      fetchPolicy: 'no-cache',
-      errorPolicy: 'ignore',
-    },
-    query: {
-      fetchPolicy: 'no-cache',
-      errorPolicy: 'all',
-    },
-    mutate: {
-      fetchPolicy: 'no-cache',
-      errorPolicy: 'all',
-    }
-  };
-
-
   return {
-    // link: httpLink.create({uri}),
-    link,
-    cache: new InMemoryCache({
-      addTypename: false
-    }),
-    connectToDevTools: true,
-    defaultOptions,
+    link: httpLink.create({uri, extractFiles}),
+    cache: new InMemoryCache(),
   };
 }
 
 @NgModule({
+  imports: [ApolloModule],
   providers: [
     {
       provide: APOLLO_OPTIONS,
