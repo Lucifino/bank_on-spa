cba527cffe1574891fe4a962963cdfaca659db42
Guiliano Bastien <gpconcepcion1@up.edu.ph>
2022-12-06 16:48:45 +0800

    env changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   graphql_url: 'https://bankon-api.azurewebsites.net/graphql',
-  prod_url: 'http://localhost:4200',
+  prod_url: 'https://kind-sand-099505e10.2.azurestaticapps.net',
   api_url: 'https://bankon-api.azurewebsites.net/'
 };
